c3e393d036c1adc9f0b7edd922274562824ab986
Anderson <andersonzhangyq@gmail.com>
2021-02-26 15:01:29 +0800

    sync

--- Q1 starter code/model.py
+++ Q1 starter code/model.py
@@ -11,3 +11,29 @@ class FCModel(nn.Module):
         x = x.flatten(1)
         output = self.fc(x)
         return output
+
+
+class CNNModel(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+        self.feature = nn.Sequential(nn.Conv2d(in_channels, 64, kernel_size=3),
+                                     nn.ReLU(),
+                                     nn.MaxPool2d(kernel_size=3, stride=2),
+                                     nn.Conv2d(64, 128, kernel_size=3),
+                                     nn.ReLU(),
+                                     nn.MaxPool2d(kernel_size=3, stride=2),
+                                     nn.Conv2d(128, 256, kernel_size=3),
+                                     nn.ReLU(),
+                                     nn.MaxPool2d(kernel_size=3, stride=2))
+        self.avg_pool = nn.AdaptiveAvgPool2d((7, 7))
+        self.classifier = nn.Sequential(nn.Linear(256 * 7 * 7,
+                                                  1024), nn.ReLU(),
+                                        nn.Linear(1024, num_classes),
+                                        nn.Softmax())
+
+    def forward(self, x):
+        x = self.feature(x)
+        x = self.avg_pool(x)
+        x = x.flatten(1)
+        output = self.classifier(x)
+        return output
--- Q1 starter code/starter_code.py
+++ Q1 starter code/starter_code.py
@@ -7,7 +7,6 @@ Some helpful code for getting started.
 
 """
 
-
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -20,8 +19,7 @@ from config import *
 
 from torch import nn, optim
 from torch.utils.tensorboard import SummaryWriter
-from model import FCModel
-
+from model import *
 
 # Gathers the meta data for the images
 paths, classes = [], []
@@ -30,36 +28,29 @@ for i, dir_ in enumerate(CLASS_LABELS):
         if (entry.is_file()):
             paths.append(entry.path)
             classes.append(i)
-            
-data = {
-    'path': paths,
-    'class': classes
-}
+
+data = {'path': paths, 'class': classes}
 
 data_df = pd.DataFrame(data, columns=['path', 'class'])
-data_df = data_df.sample(frac=1).reset_index(drop=True) # Shuffles the data
+data_df = data_df.sample(frac=1).reset_index(drop=True)  # Shuffles the data
 
 # See what the dataframe now contains
 print("Found", len(data_df), "images.")
 # If you want to see the image meta data
 print(data_df.head())
 
-
-
 # Split the data into train and test sets and instantiate our new ImageNet10 objects.
-train_split = 0.80 # Defines the ratio of train/valid data.
+train_split = 0.80  # Defines the ratio of train/valid data.
 
 # valid_size = 1.0 - train_size
-train_size = int(len(data_df)*train_split)
-
+train_size = int(len(data_df) * train_split)
 
 data_transform = transforms.Compose([
-        transforms.Resize(128),
-        transforms.CenterCrop(128),
-        transforms.ToTensor(),
-        transforms.Normalize(NORM_MEAN, NORM_STD),
-    ])
-
+    transforms.Resize(128),
+    transforms.CenterCrop(128),
+    transforms.ToTensor(),
+    transforms.Normalize(NORM_MEAN, NORM_STD),
+])
 
 dataset_train = ImageNet10(
     df=data_df[:train_size],
@@ -71,21 +62,16 @@ dataset_valid = ImageNet10(
     transform=data_transform,
 )
 
-
 # Data loaders for use during training
-train_loader = torch.utils.data.DataLoader(
-    dataset_train,
-    batch_size=64,
-    shuffle=True,
-    num_workers=2
-)
+train_loader = torch.utils.data.DataLoader(dataset_train,
+                                           batch_size=64,
+                                           shuffle=True,
+                                           num_workers=2)
 
-valid_loader = torch.utils.data.DataLoader(
-    dataset_valid,
-    batch_size=128,
-    shuffle=True,
-    num_workers=2
-)
+valid_loader = torch.utils.data.DataLoader(dataset_valid,
+                                           batch_size=128,
+                                           shuffle=True,
+                                           num_workers=2)
 
 # See what you've loaded
 print("len(dataset_train)", len(dataset_train))
@@ -95,65 +81,231 @@ print("len(train_loader)", len(train_loader))
 print("len(valid_loader)", len(valid_loader))
 
 
-# single batch training
-# prepare tensorboard
-writer = SummaryWriter()
-output_path = writer.get_logdir()
-net = FCModel(3 * 128 * 128, len(classes))
-
-# define loss and optim
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(net.parameters())
-
-
-# get one batch of training data
-train_loader = torch.utils.data.DataLoader(
-    dataset_train,
-    batch_size=64,
-    shuffle=False, # make the first batch of data consistent between executions
-    num_workers=2
-)
-train_data, train_label = next(iter(train_loader))
-
-
-# use gpu if available
-if torch.cuda.is_available():
-    print("using gpu")
-    net = net.cuda()
-    criterion = criterion.cuda()
-    train_data = train_data.cuda()
-    train_label = train_label.cuda()
-
-
-# training code
-epoch_num = 40
-train_loss = []
-val_loss = []
-for epoch_idx in range(epoch_num):
-    # zero the parameter gradients
-    optimizer.zero_grad()
-
-    # forward + backward + optimize
-    outputs = net(train_data)
-    loss = criterion(outputs, train_label)
-    train_loss.append(loss.cpu().detach().numpy().item())
-    writer.add_scalar("Loss/train", loss.cpu().detach(), epoch_idx)
-    loss.backward()
-    optimizer.step()
-
-    # validation
+def accuracy(output, target, topk=(1, )):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
     with torch.no_grad():
-        tmp = 0
-        for i, (val_data, val_label) in enumerate(valid_loader):
-            val_data = val_data.to(train_data.device)
-            val_label = val_label.to(train_label.device)
-            outputs = net(val_data)
-            loss = criterion(outputs, val_label)
-            tmp += loss.cpu().detach()
-        writer.add_scalar("Loss/val", tmp / len(valid_loader), epoch_idx)
-        val_loss.append(tmp.numpy().item() / len(valid_loader))
-    print(f"Train Epoch({epoch_idx + 1} / {epoch_num}), train_loss: {train_loss[-1]}, val_loss: {val_loss[-1]}")
+        maxk = max(topk)
+        batch_size = target.size(0)
 
-print("save loss")
-torch.save(val_loss, os.path.join(output_path, "val_loss"))
-torch.save(train_loss, os.path.join(output_path, "train_loss"))
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target[None])
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].flatten().sum(dtype=torch.float32)
+            res.append(correct_k * (100.0 / batch_size))
+        return res
+
+
+def q1_1_fc():
+    # prepare tensorboard
+    writer = SummaryWriter(comment="-q1.1-FC")
+    output_path = writer.get_logdir()
+    net = FCModel(3 * 128 * 128, len(classes))
+
+    # define loss and optim
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters())
+
+    # get one batch of training data
+    train_loader = torch.utils.data.DataLoader(
+        dataset_train,
+        batch_size=64,
+        shuffle=
+        False,  # make the first batch of data consistent between executions
+        num_workers=2)
+    train_data, train_label = next(iter(train_loader))
+
+    # use gpu if available
+    if torch.cuda.is_available():
+        print("using gpu")
+        net = net.cuda()
+        criterion = criterion.cuda()
+        train_data = train_data.cuda()
+        train_label = train_label.cuda()
+
+    # training code
+    epoch_num = 40
+    train_loss = []
+    val_loss = []
+    for epoch_idx in range(epoch_num):
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(train_data)
+        loss = criterion(outputs, train_label)
+        loss.backward()
+        optimizer.step()
+
+        train_loss.append(loss.cpu().detach().numpy().item())
+        writer.add_scalar("Loss/train", loss.cpu().detach(), epoch_idx)
+
+        # validation
+        with torch.no_grad():
+            tmp = 0
+            for i, (val_data, val_label) in enumerate(valid_loader):
+                val_data = val_data.to(train_data.device)
+                val_label = val_label.to(train_label.device)
+                outputs = net(val_data)
+                loss = criterion(outputs, val_label)
+                tmp += loss.cpu().detach()
+            writer.add_scalar("Loss/val", tmp / len(valid_loader), epoch_idx)
+            val_loss.append(tmp.numpy().item() / len(valid_loader))
+        print(
+            f"Train Epoch({epoch_idx + 1} / {epoch_num}), train_loss: {train_loss[-1]}, val_loss: {val_loss[-1]}"
+        )
+
+    print("save loss")
+    torch.save(val_loss, os.path.join(output_path, "val_loss"))
+    torch.save(train_loss, os.path.join(output_path, "train_loss"))
+
+
+def q1_1_cnn():
+    # prepare tensorboard
+    writer = SummaryWriter(comment="-q1.1-CNN")
+    output_path = writer.get_logdir()
+    net = CNNModel(3, len(classes))
+
+    # define loss and optim
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters())
+
+    # get one batch of training data
+    train_loader = torch.utils.data.DataLoader(
+        dataset_train,
+        batch_size=64,
+        shuffle=
+        False,  # make the first batch of data consistent between executions
+        num_workers=2)
+    train_data, train_label = next(iter(train_loader))
+
+    # use gpu if available
+    if torch.cuda.is_available():
+        print("using gpu")
+        net = net.cuda()
+        criterion = criterion.cuda()
+        train_data = train_data.cuda()
+        train_label = train_label.cuda()
+
+    # training code
+    epoch_num = 40
+    train_loss = []
+    val_loss = []
+    for epoch_idx in range(epoch_num):
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(train_data)
+        loss = criterion(outputs, train_label)
+        loss.backward()
+        optimizer.step()
+
+        train_loss.append(loss.cpu().detach().numpy().item())
+        writer.add_scalar("Loss/train", loss.cpu().detach(), epoch_idx)
+
+        # validation
+        with torch.no_grad():
+            tmp = 0
+            for i, (val_data, val_label) in enumerate(valid_loader):
+                val_data = val_data.to(train_data.device)
+                val_label = val_label.to(train_label.device)
+                outputs = net(val_data)
+                loss = criterion(outputs, val_label)
+                tmp += loss.cpu().detach()
+            writer.add_scalar("Loss/val", tmp / len(valid_loader), epoch_idx)
+            val_loss.append(tmp.numpy().item() / len(valid_loader))
+        print(
+            f"Train Epoch({epoch_idx + 1} / {epoch_num}), train_loss: {train_loss[-1]}, val_loss: {val_loss[-1]}"
+        )
+
+    print("save loss")
+    torch.save(val_loss, os.path.join(output_path, "cnn_val_loss"))
+    torch.save(train_loss, os.path.join(output_path, "cnn_train_loss"))
+
+
+def q1_2():
+    # prepare tensorboard
+    writer = SummaryWriter(comment="-q1.2-CNN")
+    output_path = writer.get_logdir()
+    net = CNNModel(3, len(classes))
+
+    # define loss and optim
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters())
+
+    device = torch.device('cpu')
+    # use gpu if available
+    if torch.cuda.is_available():
+        print("using gpu")
+        net = net.cuda()
+        criterion = criterion.cuda()
+        device = torch.device('cuda')
+
+    # training code
+    epoch_num = 100
+    train_loss = []
+    val_loss = []
+    step = 0
+    for epoch_idx in range(epoch_num):
+        tmp = 0
+        train_label_gt = []
+        train_label_pred = []
+        for i, (train_data, train_label) in enumerate(train_loader):
+            train_data = train_data.to(device)
+            train_label = train_label.to(device)
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(train_data)
+            loss = criterion(outputs, train_label)
+            loss.backward()
+            optimizer.step()
+
+            train_label_gt.append(train_label.cpu())
+            train_label_pred.append(outputs.cpu().detach())
+            tmp += loss.cpu().detach()
+            writer.add_scalar("Loss/train_per_batch",
+                              loss.cpu().detach(), step)
+            step += 1
+        train_loss.append(tmp.numpy().item() / len(train_loader))
+        writer.add_scalar(
+            "acc/train",
+            accuracy(torch.cat(train_label_pred), torch.cat(train_label_gt))[0],
+            epoch_idx)
+        writer.add_scalar("Loss/train", tmp / len(train_loader), epoch_idx)
+
+        # validation
+        with torch.no_grad():
+            tmp = 0
+            val_label_gt = []
+            val_label_pred = []
+            for i, (val_data, val_label) in enumerate(valid_loader):
+                val_data = val_data.to(device)
+                val_label = val_label.to(device)
+                outputs = net(val_data)
+                loss = criterion(outputs, val_label)
+                tmp += loss.cpu().detach()
+                val_label_gt.append(val_label.cpu())
+                val_label_pred.append(outputs.cpu().detach())
+            writer.add_scalar(
+                "acc/train",
+                accuracy(torch.cat(val_label_pred), torch.cat(val_label_gt))[0],
+                epoch_idx)
+            writer.add_scalar("Loss/val", tmp / len(valid_loader), epoch_idx)
+            val_loss.append(tmp.numpy().item() / len(valid_loader))
+        print(
+            f"Train Epoch({epoch_idx + 1} / {epoch_num}), train_loss: {train_loss[-1]}, val_loss: {val_loss[-1]}"
+        )
+
+    print("save loss")
+    torch.save(val_loss, os.path.join(output_path, "cnn_val_loss"))
+    torch.save(train_loss, os.path.join(output_path, "cnn_train_loss"))
+
+
+# q1_1_fc()
+# q1_1_cnn()
+q1_2()
